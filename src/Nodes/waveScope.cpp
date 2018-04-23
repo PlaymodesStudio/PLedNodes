8ec4f7d4a298b1fa9529e7c0875857c63f13abb2
eduardfrigola <eduardfrigola@yahoo.com>
2018-04-23 16:50:14 +0200

    texture scope fixed dynamic input behaviour, works with presets

--- src/Nodes/waveScope.cpp
+++ src/Nodes/waveScope.cpp
@@ -11,48 +11,10 @@
 
 waveScope::waveScope() : ofxOceanodeNodeModelExternalWindow("Texture Scope"){
     
-    texturesInput.resize(1);
-    for(int i = 0; i < 1; i++){
-        parameters->add(texturesInput[i].set("Group In " + ofToString(i), nullptr));
-    }
-    ifNewCreatedCheker.push_back(false);
-    
-    listeners[texturesInput.back().getName()] = texturesInput.back().newListener([&](ofTexture* &tex){
-        inputListener(0);
+    texturesInput.setup(parameters, ofParameter<ofTexture*>("Group In", nullptr));
+    listener = texturesInput.parameterGroupChanged.newListener([&](){
+        ofNotifyEvent(parameterGroupChanged);
     });
-}
-
-void waveScope::inputListener(int index){
-    if(texturesInput[index].get() != nullptr && !ifNewCreatedCheker[index]){
-        texturesInput.push_back(ofParameter<ofTexture*>());
-        ifNewCreatedCheker.push_back(false);
-        int newCreatedIndex = texturesInput.size()-1;
-        parameters->add(texturesInput.back().set("Group In " + ofToString(newCreatedIndex), nullptr));
-        listeners[texturesInput.back().getName()] = texturesInput.back().newListener([&, newCreatedIndex](ofTexture* &tex){
-            inputListener(newCreatedIndex);
-        });
-        ifNewCreatedCheker[index] = true;
-        ofNotifyEvent(parameterGroupChanged);
-    }
-    else if(texturesInput[index].get() == nullptr){
-//        parameters->remove(texturesInput[index].getEscapedName());
-//        listeners.erase(texturesInput[index].getName());
-//        texturesInput.erase(texturesInput.begin()+index);
-//        ifNewCreatedCheker.erase(ifNewCreatedCheker.begin()+index);
-        parameters->remove(texturesInput.back().getEscapedName());
-        listeners.erase(texturesInput.back().getName());
-        texturesInput.erase(texturesInput.end()-1);
-        ifNewCreatedCheker.erase(ifNewCreatedCheker.end()-1);
-        ifNewCreatedCheker[index] = false;
-//        for(int ii = 0; ii < texturesInput.size(); ii++){
-            //parameters->remove(texturesInput[ii].getEscapedName());
-//        }
-//        for(int ii = 0; ii < texturesInput.size(); ii++){
-//            texturesInput[ii].setName("Group In " + ofToString(ii));
-//            parameters->add(texturesInput[ii]);
-//        }
-        ofNotifyEvent(parameterGroupChanged);
-    }
 }
 
 void waveScope::drawInExternalWindow(ofEventArgs &e){
@@ -63,8 +25,8 @@ void waveScope::drawInExternalWindow(ofEventArgs &e){
     
     
     int numActiveGroups = 0;
-    for(auto texture : texturesInput){
-        if(texture != nullptr){
+    for(auto texture : texturesInput.getParameters()){
+        if(texture.second != nullptr){
             numActiveGroups++;
         }
     }
@@ -74,10 +36,10 @@ void waveScope::drawInExternalWindow(ofEventArgs &e){
         int oscDrawIndex = 0;
         
         //Draw the groups
-        for(int i = 0; i < texturesInput.size(); i++){
-            if(texturesInput[i] != nullptr){
+        for(auto texture : texturesInput.getParameters()){
+            if(texture.second != nullptr){
                 int topPosition = (elementHeight * oscDrawIndex);
-                texturesInput[i].get()->draw(0,topPosition, ofGetWidth(), elementHeight);
+                texture.second.get()->draw(0,topPosition, ofGetWidth(), elementHeight);
                 ofPushStyle();
                 ofSetColor(ofColor::indianRed);
                 ofNoFill();
