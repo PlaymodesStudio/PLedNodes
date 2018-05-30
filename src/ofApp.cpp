2047e4d5435d6339f29687f8166eb0be7b02b319
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2018-05-30 11:34:11 +0200

    Added basic config file functionality

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,11 +7,68 @@
 #include "scriptModule.h"
 #include "vector_op_registrator.h"
 
+#include "ofxOceanodeBPMController.h"
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofDisableArbTex();
     ofSetVerticalSync(true);
-    ofSetFrameRate(33);
+    ofSetEscapeQuitsApp(false);
+    
+    auto result = ofSystemLoadDialog("Select Generator File", false, ofToDataPath("../"));
+    if(ofSplitString(result.getPath(), ".").back() != "generator"){
+        ofExit();
+    }
+    
+    float bpm = 120;
+    
+    ofJson json = ofLoadJson(result.getPath());
+    if(!json.empty()){
+        string name = json["Name"];
+        ofSetWindowTitle(name);
+        bpm = json["BPM"];
+        int frameRate = json["FPS"];
+        ofSetFrameRate(frameRate);
+        
+        int syphonSenders = json["Syphon"];
+        
+//            int numSyphonServers = xml.getIntValue("SyphonSenders");
+//            for(int i = 0; i < numSyphonServers; i++){
+//                bool invert = xml.getBoolValue("SyphonSender"+ ofToString(i+1) + "InvertTexture");
+//                string syphonName = xml.getValue("SyphonSender"+ ofToString(i+1) + "Name");
+//                senderModules.push_back(new senderManager(i+1, invert, syphonName));
+//            }
+//
+        auto mainPos = json["MainWindowPos"];
+        if(mainPos.size() == 2){
+            ofSetWindowPosition(ofToInt(mainPos[0]), ofToInt(mainPos[1]));
+        }
+
+        auto mainSize = json["MainWindowSize"];
+        if(mainSize.size() == 2){
+            ofSetWindowShape(ofToInt(mainSize[0]), ofToInt(mainSize[1]));
+        }
+
+        auto prevPos = json["PrevWindowPos"];
+        if(prevPos.size() == 2){
+//            prevWinRect.setPosition(ofToInt(prevPos[0]), ofToInt(prevPos[1]));
+        }
+        
+        auto prevSize = json["PrevWindowSize"];
+        if(prevSize.size() == 2){
+//            prevWinRect.setSize(ofToInt(prevSize[0]), ofToInt(prevSize[1]));
+        }
+
+        int unifierNum = json["TextureUnifierNumber"];
+        int unifierSpacing = json["TextureUnifierSpacing"];
+//            if(unifierNum > 0){
+//                new textureUnifier(unifierNum, unifierSpacing);
+//            }
+        
+        if(json["TextureRecorder"]){
+            //                new dataRecorder();
+        }
+    }
     
     auto reg = make_shared<ofxOceanodeNodeRegistry>();
     auto treg = make_shared<ofxOceanodeTypesRegistry>();
@@ -31,6 +88,7 @@ void ofApp::setup(){
     canvas.setup();
     
     controls = new ofxOceanodeControls(container);
+    controls->get<ofxOceanodeBPMController>()->setBPM(bpm);
 }
 
 //--------------------------------------------------------------
