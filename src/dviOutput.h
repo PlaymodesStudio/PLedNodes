bc1900d347416cf1512b51801eb2fb4b1a6186c4
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2019-03-13 13:20:01 +0100

    update dviOutput, add masterfader, and draw centered

--- src/dviOutput.h
+++ src/dviOutput.h
@@ -17,6 +17,7 @@ public:
     
     void setup() override{
         parameters->add(texture.set("Texture", nullptr));
+        addParameterToGroupAndInfo(masterFader.set("Master Fader", 1, 0, 1)).isSavePreset = false;
     };
     
     void presetHasLoaded() override{
@@ -27,18 +28,24 @@ public:
         ofGetMainLoop()->setCurrentWindow(currentWin);
     }
     
+    void
+    
 private:
     void drawInExternalWindow(ofEventArgs &e) override{
         ofBackground(0);
         if(texture.get() != nullptr){
+            ofPushStyle();
+            ofSetRectMode(OF_RECTMODE_CENTER);
+            ofSetColor(masterFader*255);
             texture.get()->setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
-            texture.get()->draw(0, 100);
+            texture.get()->draw(ofGetWidth()/2, ofGetHeight()/2);
+            ofPopStyle();
         }
     }
     
     
-    
     ofParameter<ofTexture*> texture;
+    ofParameter<float> masterFader;
 };
 
 #endif /* dviOutput_h */
